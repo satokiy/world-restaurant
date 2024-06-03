feab7232c567075d384bb1d1a3d94d423fd087c2
satokiy <43191933+satokiy@users.noreply.github.com>
2024-06-03 21:33:59 +0900

    feat: add 2D globe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Globe from "./components/Globe";
+import Globe2D from "./components/Globe2D";
 
 const queryClient = new QueryClient();
 
@@ -9,16 +10,23 @@ function App() {
     console.log(data);
   };
   const handleMouseOver = (data: any) => {
-    console.log("Mouse Over", data.properties.SOVEREIGNT);
+    console.log(data.properties.SOVEREIGNT);
   };
   const handleMouseOut = (data: any) => {
-    console.log("Mouse Out", data.properties.SOVEREIGNT);
+    console.log(data.properties.SOVEREIGNT);
   };
 
   return (
     <div style={{ width: "100vw" }}>
       <QueryClientProvider client={queryClient}>
-        <Globe
+        {
+          /* <Globe
+          onCountryClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        /> */
+        }
+        <Globe2D
           onCountryClick={handleClick}
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
